c8f0183b6fa2472247e758b6e635173abc4b3a20
Tran Thanh <tranthanhvt0982@gmail.com>
2026-02-16 21:38:37 +0700

    first commit

---
+++ src/main/java/com/example/testauthservice/TestAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testauthservice/TestAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
